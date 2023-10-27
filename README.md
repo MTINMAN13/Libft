096f15790dc40da8bd2d68d353c4ddedf2e28d0e
Apple <apple@Apple-MacBook.local>
2023-10-27 16:43:27 +0200

    100% no bonus

--- README.md
+++ README.md
@@ -7,3 +7,18 @@
 TODO :
 1) check norm
 2) bonys items
+
+
+## contents
+#### writing
+
+1) PUTNBR | void ft_putnbr_fd(int n, int fd);
+2) PUTSTR | ft_putstr_fd(char, fd)
+3) PUTCHAR | ft_putchar_fd(char, fd)
+
+### mallocs
+
+
+
+
+
--- ft_memmove.c
+++ ft_memmove.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memmove.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mman <mman@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: apple <apple@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/20 16:16:12 by mman              #+#    #+#             */
-/*   Updated: 2023/10/27 16:04:48 by mman             ###   ########.fr       */
+/*   Updated: 2023/10/27 16:34:33 by apple            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,7 +27,12 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 		while (len-- > 0)
 			destination[len] = source[len];
 	else
+	{
 		while (i < len)
-			destination[i] = source[i++];
+		{
+			destination[i] = source[i];
+			i++;
+		}
+	}
 	return (dst);
 }
