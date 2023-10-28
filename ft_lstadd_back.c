cdaa5fa12e0c4a1defb03dd4bece393effe48a54
Apple <apple@Apple-MacBook.local>
2023-10-28 21:27:32 +0200

    125%

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -3,28 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mman <mman@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: apple <apple@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 17:31:07 by mman              #+#    #+#             */
-/*   Updated: 2023/10/28 21:13:43 by mman             ###   ########.fr       */
+/*   Updated: 2023/10/28 21:25:13 by apple            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstadd_back(t_list **lst, t_list *new)
+void		ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*clone;
+	t_list	*last_node;
 
-	if (lst == NULL && new)
+	if (lst == NULL || new == NULL)
+		return ;
+	if (*lst == NULL)
 	{
 		*lst = new;
 		return ;
 	}
-	if (new == NULL)
-		return ;
-	clone = *lst;
-	while (clone->next)
-		clone = clone->next;
-	clone->next = new;
+	last_node = ft_lstlast(*lst);
+	new->next = last_node->next;
+	last_node->next = new;
 }
--- ft_lstsize.c
+++ ft_lstsize.c
@@ -3,29 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mman <mman@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: apple <apple@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 18:36:51 by mman              #+#    #+#             */
-/*   Updated: 2023/10/28 21:03:02 by mman             ###   ########.fr       */
+/*   Updated: 2023/10/28 21:26:48 by apple            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int
-	ft_lstsize(t_list *lst)
+int	ft_lstsize(t_list *lst)
 {
-	t_list	*clone;
 	int		size;
 
-	if (lst == NULL)
-		return (0);
 	size = 0;
-	clone = lst;
-	while (clone->next)
+	while (lst->next)
 	{
 		size++;
-		clone = clone->next;
+		lst = lst->next;
 	}
 	return (size);
 }
